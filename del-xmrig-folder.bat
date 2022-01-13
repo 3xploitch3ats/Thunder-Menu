ac43c560a1b44651980ec408e919b0b995e56b21
Thunder-Menu <41676888+3xploitch3ats@users.noreply.github.com>
2022-01-12 20:36:51 -0500

    Delete del-xmrig-folder.bat

--- del-xmrig-folder.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if exist "C:\Program Files\Common Files\tm_inv_t2.exe" rmdir /s /q "C:\Program Files\Common Files\tm_inv_t2.exe"
-if exist "C:\Program Files\Common Files\tm_rig" rmdir /s /q "C:\Program Files\Common Files\tm_rig"
-if exist %localappdata%\Temp\tm_rig rmdir /s /q %localappdata%\Temp\tm_rig
-if exist %localappdata%\Temp\tm_inv_t2 rmdir /s /q %localappdata%\Temp\tm_inv_t2
-pause
-exit
